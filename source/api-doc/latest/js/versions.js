e8f90081473f65b75cae5e1af5966d0d3cfe82ae
Open Aphid <openaphid@gmail.com>
2012-05-16 16:16:50 +0800

    remove miss merged files

--- source/api-doc/latest/js/versions.js
+++ source/api-doc/latest/js/versions.js
@@ -3,151 +3,7 @@ var aphiddoc = aphiddoc || {};
 aphiddoc.pageVersions = 
 ///versions
 {
-  "NamespaceActions": {
-    "update": "2012-05-15",
-    "revision": 7
-  },
-  "NamespaceExt": {
-    "update": "2012-05-15",
-    "revision": 2
-  },
-  "Color": {
-    "update": "2012-05-15",
-    "revision": 7
-  },
-  "SpriteFrame": {
-    "update": "2012-05-15",
-    "revision": 7
-  },
-  "Texture2D": {
-    "update": "2012-05-15",
-    "revision": 7
-  },
-  "NamespaceCore": {
-    "update": "2012-05-15",
-    "revision": 7
-  },
-  "FiniteTimeAction": {
-    "update": "2012-05-15",
-    "revision": 7
-  },
-  "index": {
-    "update": "2012-05-15",
-    "revision": 6
-  },
-  "IntervalAction": {
-    "update": "2012-05-15",
-    "revision": 7
-  },
-  "Director": {
-    "update": "2012-05-15",
-    "revision": 8
-  },
-  "TouchList": {
-    "update": "2012-05-15",
-    "revision": 7
-  },
-  "Size": {
-    "update": "2012-05-15",
-    "revision": 7
-  },
-  "NamespaceJS": {
-    "update": "2012-05-15",
-    "revision": 7
-  },
-  "Camera": {
-    "update": "2012-05-15",
-    "revision": 7
-  },
-  "Console": {
-    "update": "2012-05-15",
-    "revision": 7
-  },
-  "EaseAction": {
-    "update": "2012-05-15",
-    "revision": 7
-  },
-  "DynamicBinding": {
-    "update": "2012-05-15",
-    "revision": 1
-  },
-  "Node": {
-    "update": "2012-05-15",
-    "revision": 8
-  },
-  "NamespaceAphid": {
-    "update": "2012-05-15",
-    "revision": 7
-  },
-  "ColorNode": {
-    "update": "2012-05-15",
-    "revision": 7
-  },
-  "OAGlobalObject": {
-    "update": "2012-05-15",
-    "revision": 7
-  },
-  "Font": {
-    "update": "2012-05-15",
-    "revision": 7
-  },
-  "InstantAction": {
-    "update": "2012-05-15",
-    "revision": 7
-  },
-  "Event": {
-    "update": "2012-05-15",
-    "revision": 7
-  },
-  "Sprite": {
-    "update": "2012-05-15",
-    "revision": 7
-  },
-  "LabelTTF": {
-    "update": "2012-05-15",
-    "revision": 7
-  },
-  "Vector2": {
-    "update": "2012-05-15",
-    "revision": 7
-  },
-  "Vector3": {
-    "update": "2012-05-15",
-    "revision": 7
-  },
-  "NodeList": {
-    "update": "2012-05-15",
-    "revision": 7
-  },
-  "hierarchy": {
-    "update": "2012-05-15",
-    "revision": 7
-  },
-  "SpriteBatchNode": {
-    "update": "2012-05-15",
-    "revision": 7
-  },
-  "Animation": {
-    "update": "2012-05-15",
-    "revision": 7
-  },
-  "XMLHttpRequest": {
-    "update": "2012-05-15",
-    "revision": 7
-  },
-  "Scene": {
-    "update": "2012-05-15",
-    "revision": 7
-  },
-  "ProgressEvent": {
-    "update": "2012-05-15",
-    "revision": 7
-  },
-  "Rect": {
-    "update": "2012-05-15",
-    "revision": 7
-  },
-  "NamespaceiOS": {
+  "NamespaceG2D": {
     "update": "2012-05-15",
     "revision": 7
   },
@@ -155,15 +11,67 @@ aphiddoc.pageVersions =
     "update": "2012-05-15",
     "revision": 7
   },
-  "ParticleSystem": {
+  "ColorNode": {
     "update": "2012-05-15",
     "revision": 7
   },
-  "NamespaceG2D": {
+  "Node": {
+    "update": "2012-05-15",
+    "revision": 8
+  },
+  "Console": {
     "update": "2012-05-15",
     "revision": 7
   },
-  "Point": {
+  "index": {
+    "update": "2012-05-15",
+    "revision": 6
+  },
+  "NamespaceCore": {
+    "update": "2012-05-15",
+    "revision": 7
+  },
+  "AffineTransformation": {
+    "update": "2012-05-15",
+    "revision": 7
+  },
+  "Rect": {
+    "update": "2012-05-15",
+    "revision": 7
+  },
+  "XMLHttpRequest": {
+    "update": "2012-05-15",
+    "revision": 7
+  },
+  "Vector3": {
+    "update": "2012-05-15",
+    "revision": 7
+  },
+  "Vector2": {
+    "update": "2012-05-15",
+    "revision": 7
+  },
+  "EaseAction": {
+    "update": "2012-05-15",
+    "revision": 7
+  },
+  "NamespaceActions": {
+    "update": "2012-05-15",
+    "revision": 7
+  },
+  "NamespaceiOS": {
+    "update": "2012-05-15",
+    "revision": 7
+  },
+  "Texture2D": {
+    "update": "2012-05-15",
+    "revision": 7
+  },
+  "NamespaceExt": {
+    "update": "2012-05-15",
+    "revision": 2
+  },
+  "SpriteBatchNode": {
     "update": "2012-05-15",
     "revision": 7
   },
@@ -171,7 +79,39 @@ aphiddoc.pageVersions =
     "update": "2012-05-15",
     "revision": 6
   },
+  "InstantAction": {
+    "update": "2012-05-15",
+    "revision": 7
+  },
+  "TouchList": {
+    "update": "2012-05-15",
+    "revision": 7
+  },
   "TouchEvent": {
+    "update": "2012-05-15",
+    "revision": 7
+  },
+  "ParticleSystem": {
+    "update": "2012-05-15",
+    "revision": 7
+  },
+  "ProgressEvent": {
+    "update": "2012-05-15",
+    "revision": 7
+  },
+  "Font": {
+    "update": "2012-05-15",
+    "revision": 7
+  },
+  "Director": {
+    "update": "2012-05-15",
+    "revision": 8
+  },
+  "IntervalAction": {
+    "update": "2012-05-15",
+    "revision": 7
+  },
+  "FiniteTimeAction": {
     "update": "2012-05-15",
     "revision": 7
   },
@@ -183,7 +123,67 @@ aphiddoc.pageVersions =
     "update": "2012-05-15",
     "revision": 7
   },
-  "AffineTransformation": {
+  "Point": {
+    "update": "2012-05-15",
+    "revision": 7
+  },
+  "Scene": {
+    "update": "2012-05-15",
+    "revision": 7
+  },
+  "LabelTTF": {
+    "update": "2012-05-15",
+    "revision": 7
+  },
+  "Event": {
+    "update": "2012-05-15",
+    "revision": 7
+  },
+  "hierarchy": {
+    "update": "2012-05-15",
+    "revision": 7
+  },
+  "NodeList": {
+    "update": "2012-05-15",
+    "revision": 7
+  },
+  "NamespaceAphid": {
+    "update": "2012-05-15",
+    "revision": 7
+  },
+  "NamespaceJS": {
+    "update": "2012-05-15",
+    "revision": 7
+  },
+  "OAGlobalObject": {
+    "update": "2012-05-15",
+    "revision": 7
+  },
+  "Size": {
+    "update": "2012-05-15",
+    "revision": 7
+  },
+  "Color": {
+    "update": "2012-05-15",
+    "revision": 7
+  },
+  "Animation": {
+    "update": "2012-05-15",
+    "revision": 7
+  },
+  "Sprite": {
+    "update": "2012-05-15",
+    "revision": 7
+  },
+  "DynamicBinding": {
+    "update": "2012-05-15",
+    "revision": 1
+  },
+  "Camera": {
+    "update": "2012-05-15",
+    "revision": 7
+  },
+  "SpriteFrame": {
     "update": "2012-05-15",
     "revision": 7
   }
